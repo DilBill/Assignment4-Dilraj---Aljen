c662a6b088c979eaba8faa2dce24c8ce51c75993
DilBil <dilrajbola25@gmail.com>
2023-12-05 17:01:05 -0500

    GuI interface Close to done

--- GUI.py
+++ GUI.py
@@ -43,6 +43,38 @@ entryBox3.place(x=110,y=150)
 label4 = ttk.Label(win, text="Car model")
 label4.place(x= 5, y=200)
 
+# Set the default value for the ComboBox
+entryBox4 = ttk.Entry(win)# Set the default value
+entryBox4.place(x=110,y=200)
+
+entryBox5 = ttk.Entry(win)
+entryBox5.place(x= 380, y=380)
+
+addBtn = ttk.Button(win, text="Add Car", command=action)
+addBtn.place(x=150,y=280)
+
+updateBtn = ttk.Button(win,text="Update Car",command=action)
+updateBtn.place(x=450, y=280)
+
+deleteBtn = ttk.Button(win,text="Delete Car",command=action)
+deleteBtn.place(x=300,y=280)
+
+findBtn = ttk.Button(win,text="Find Car",command=action)
+findBtn.place(x= 275,y=380)
+
+showBtn = ttk.Button(win,text="Show All",command=action)
+showBtn.place(x=450,y=230)
+
+option = tk.IntVar
+
+radioBtn1 = ttk.Radiobutton(win,text="By Model",variable= option,value=1)
+radioBtn1.place(x=350,y=340)
+
+radioBtn2 = ttk.Radiobutton(win,text="By Vin",variable=option,value=2)
+radioBtn2.place(x=450,y=340)
+
+displayBox = tk.Text(win,bg="white",width=50,height=10,fg="black")
+displayBox.place(x=325,y=75)
 
 win.mainloop()
 
