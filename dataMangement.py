400f2239bf5086d9cddd61b855f113f565dd6430
DilBil <dilrajbola25@gmail.com>
2023-12-06 18:58:04 -0500

    Add method completed

--- dataMangement.py
+++ dataMangement.py
@@ -32,11 +32,25 @@ class Manager:
     def delete():
         pass
     
-    def add():
-        pass 
+    def add(year,make,model,vin):
+        data =[]
+        with open('CarInvetory.json', 'r') as jsonfile:
+            data = json.load(jsonfile)
+            new_car = {"Year": year, "Make": make, "Model": model, "Vin": vin}
+            print(type(data))
+            data.append(new_car)
+        with open('CarInvetory.json','w') as file:
+
+            json.dump(data,file,separators=(', ',': '))
+        
     
        
         
 
 
 Manager.find("R34 Skyline")
+year = "2023"
+make = "Honda"
+model = "Civic"
+vin = "1HMNG52CVNM110297"
+Manager.add(year,make,model,vin)
